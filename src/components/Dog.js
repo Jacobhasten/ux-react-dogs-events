e8e7ed123699afb60cab1e3dcd1677fe74e81f49
PUT YOUR NAME HERE <izzyhasten@gmail.com>
2020-05-28 12:51:29 -0400

    handin event handling dogs

--- src/components/Dog.js
+++ src/components/Dog.js
@@ -2,7 +2,20 @@
 import React from "react";
 
 function Dog(props) {
-  return <div>{props.dog.name}</div>;
+  const handleClick = () => alert(`You pet ${props.dog.name}`)
+  return <div>
+    <p>{props.dog.name}</p>
+    <button onClick={handleClick}>Pet Dog</button>
+    </div>;
 }
 
 export default Dog;
+
+//Now add a button to the Dog component to pet the dog.
+ 
+// Create a method, and wire it up to the button’s onClick.  
+ 
+// This button should trigger an alert like,  alert("You pet scruffles!")
+// Use the dog’s name from the props object.
+ 
+// light bulb Hint: All of the code for this should be inside the Dog component.
